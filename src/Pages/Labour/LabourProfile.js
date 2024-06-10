290f96e49edc8b79b1526edf4bccd9e40c0f29a6
T-Luxshan <luckybraveboys@gmail.com>
2024-06-11 00:34:07 +0530

    profle photo bug fixed

--- src/Pages/Labour/LabourProfile.js
+++ src/Pages/Labour/LabourProfile.js
@@ -11,8 +11,6 @@ import CallIcon from '@mui/icons-material/Call';
 import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
 import Report from '../../Components/Report';
 
-
-// Import the reviews JSON data
 import reviewsData from './MyReviews.json';
 import AboutMeModel from '../../Components/AboutMeModel';
 
@@ -67,17 +65,17 @@ const LabourProfile = () => {
     setExperience(20);
     setAvgRating(4.8);
     setName('Luxshan Thuraisingam');
-    setReviews(reviewsData); 
-    setAboutMe(" Experienced Driver with over two decades of dedicated service since the year 2000.Possessing a strong track record of safe driving, punctuality, and excellent knowledge of local and regional routes.")
+    setReviews(reviewsData);
+    setAboutMe(" Experienced Driver with over two decades of dedicated service since the year 2000. Possessing a strong track record of safe driving, punctuality, and excellent knowledge of local and regional routes.");
   }, []);
 
   const handleReviewClick = (review) => {
     console.log(review);
   };
 
-  const handleAboutMe = (abtME) =>{
+  const handleAboutMe = (abtME) => {
     setAboutMe(abtME);
-  }
+  };
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -92,9 +90,11 @@ const LabourProfile = () => {
                 backgroundImage: `url(${require("../../Assets/Ellipse3.png")})`,
                 backgroundRepeat: 'no-repeat',
                 backgroundSize: '840px 200px',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'left'
               }}>
-                <Avatar alt="Labour" src="../../Images/app-logo3.png"
-                  sx={{ width: 150, height: 150 }} />
+                <Avatar alt="Labour" src={require('../../Images/findMe.png')} sx={{ width: 150, height: 150 }} />
                 <Typography variant="h4" sx={{ fontWeight: 'bold', color: 'black', mb: '5px', mt: '20px' }}>
                   {name}
                 </Typography>
@@ -122,23 +122,21 @@ const LabourProfile = () => {
                 mb: 2,
                 backgroundColor: 'white', pt: 2, pl: 3, pr: 3, borderRadius: 4,
               }}>
-                <Box sx={{display:'flex', flexDirection:'row', mb: '5px', mt: '20px' }}>
-                <Typography variant="h5" sx={{ fontWeight: 'bold', color: '#FE9E0D'}}>
-                  About me
-                  
-                </Typography>
-                <AboutMeModel aboutMe={aboutMe} onAboutMeChange={handleAboutMe}/>
-                </Box>
-                {aboutMe ? 
-                  <Typography sx={{ fontWeight: '500', color: 'black', mb: '5px' }}>
-                  {aboutMe}
+                <Box sx={{ display: 'flex', flexDirection: 'row', mb: '5px', mt: '20px' }}>
+                  <Typography variant="h5" sx={{ fontWeight: 'bold', color: '#FE9E0D' }}>
+                    About me
                   </Typography>
-                :
-                <Typography sx={{ fontWeight: '500', color: 'grey', mb: '5px' }}>
-                   Say something about yourself, So customer will know about you.
-                </Typography>
+                  <AboutMeModel aboutMe={aboutMe} onAboutMeChange={handleAboutMe} />
+                </Box>
+                {aboutMe ?
+                  <Typography sx={{ fontWeight: '500', color: 'black', mb: '5px' }}>
+                    {aboutMe}
+                  </Typography>
+                  :
+                  <Typography sx={{ fontWeight: '500', color: 'grey', mb: '5px' }}>
+                    Say something about yourself, So customer will know about you.
+                  </Typography>
                 }
-                
 
                 <Typography sx={{ fontWeight: 'bold', color: '#FE9E0D', mt: '20px' }}>
                   Contact details
@@ -186,7 +184,6 @@ const LabourProfile = () => {
                   </Typography>
                 </Box>
               </Box>
-
 
             </MainBox>
           </Grid>
