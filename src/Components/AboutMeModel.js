56193113bc3c08ce4282f0348873b524ab65cb4f
T-Luxshan <luckybraveboys@gmail.com>
2024-06-12 14:49:45 +0530

    update profile connected with backend

--- src/Components/AboutMeModel.js
+++ src/Components/AboutMeModel.js
@@ -18,9 +18,12 @@ import {
   ListItemText,
   Select,
   Checkbox,
+  Typography,
 } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import EditIcon from '@mui/icons-material/Edit';
+import { updateProfile } from '../Service/LabourHomeService';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -54,24 +57,56 @@ const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="right" ref={ref} {...props} />;
 });
 
-const AboutMeModel = ({ AboutMe, onAboutMeChange, onLanguageChange }) => {
+const AboutMeModel = ({onAboutMeChange, onLanguageChange, onGenderChange }) => {
   const [open, setOpen] = useState(false);
   const [aboutMe, setAboutMe] = useState(AboutMe);
   const [language, setLanguage] = useState([]);
+  const [gender, setGender] = React.useState('');
+  const [updated, setUpdated] = React.useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  // const [email, setEmail] = useState(localStorage.getItem('userEmail'));
+  const [email, setEmail] = useState("sophiawilliams@example.com");
+  const [error, setError] = useState('');
   
+
+  const handleGender = (event) => {
+    setGender(event.target.value);
+    onGenderChange(event.target.value);
+  };
 
   const handleAboutMeChange = (event) => {
     setAboutMe(event.target.value);
     onAboutMeChange(event.target.value);
   };
 
-  const handleClickOpen = () => setOpen(true);
+  const handleClickOpen = () => {
+    setOpen(true);
+    setUpdated(false);
+    setError('');
+  };
+
   const handleCancel = () => setOpen(false);
 
   const handleSave = () => {
-    // Implement backend save code here.
-    if(aboutMe != '' && language.length != 0) // set errors
-      setOpen(false); // Close dialog after save
+
+    if (!aboutMe && language.length === 0 && !gender) {
+      setError("Please fill out at least one field");
+      return;
+    }
+    console.log(language);
+
+    setIsLoading(true);
+    updateProfile(email, aboutMe, gender, language)
+      .then(res => {
+        setUpdated(true);
+        setIsLoading(false);
+        setOpen(false);
+      })
+      .catch(err => {
+        setError("Profile update failed");
+        console.log(err);
+        setIsLoading(false);
+      });
   };
 
   const handleLanguageChange = (event) => {
@@ -84,7 +119,6 @@ const AboutMeModel = ({ AboutMe, onAboutMeChange, onLanguageChange }) => {
     onLanguageChange(
       typeof value === 'string' ? value.split(',') : value,
     );
-
   };
 
   return (
@@ -103,47 +137,71 @@ const AboutMeModel = ({ AboutMe, onAboutMeChange, onLanguageChange }) => {
             aria-describedby="alert-dialog-slide-aboutMe"
             sx={{ backgroundPosition: 'center' }}
           >
-            <DialogTitle>About Me</DialogTitle>
+            <DialogTitle>Personal details</DialogTitle>
             <DialogContent>
               <DialogContentText id="alert-dialog-slide-aboutMe">
-                Say something about yourself, so customers will know about you.
+               Please add your personal details, so customers can know about you.
               </DialogContentText>
               <Box sx={{ minWidth: 120, my: 1 }}>
-                <FormControl fullWidth> 
-                  <TextField
-                    label="About Me"
-                    variant="outlined"
-                    value={aboutMe}
-                    onChange={handleAboutMeChange}
-                    multiline
-                    rows={2}
-                    required
-                    placeholder="Say something about yourself"
-                    sx={{ mt: 2 }}
-                  />
+                {isLoading ? 
+                  <Box sx={{m:10, ml:25}}>
+                    <CircularProgress color='primary'/>
+                  </Box>
+                  :(
+                  <>
+                    <FormControl fullWidth> 
+                      <TextField
+                        label="About Me"
+                        variant="outlined"
+                        value={aboutMe}
+                        onChange={handleAboutMeChange}
+                        multiline
+                        rows={2}
+                        placeholder="Say something about yourself"
+                        sx={{ mt: 2 }}
+                      />
 
-                  <FormControl fullWidth sx={{ mt: 2 }}>
-                    <InputLabel id="language-select-label">Languages</InputLabel>
-                    <Select
-                      labelId="language-select-label"
-                      id="language-select"
-                      multiple
-                      value={language}
-                      required
-                      onChange={handleLanguageChange}
-                      input={<OutlinedInput label="Languages" />}
-                      renderValue={(selected) => selected.join(', ')}
-                      MenuProps={MenuProps}
-                    >
-                      {languages.map((lang) => (
-                        <MenuItem key={lang} value={lang}>
-                          <Checkbox checked={language.indexOf(lang) > -1} />
-                          <ListItemText primary={lang} />
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                </FormControl>
+                      <FormControl fullWidth sx={{ mt: 2 }}>
+                        <InputLabel id="language-select-label">Languages</InputLabel>
+                        <Select
+                          labelId="language-select-label"
+                          id="language-select"
+                          multiple
+                          value={language}
+                          onChange={handleLanguageChange}
+                          input={<OutlinedInput label="Languages" />}
+                          renderValue={(selected) => selected.join(', ')}
+                          MenuProps={MenuProps}
+                        >
+                          {languages.map((lang) => (
+                            <MenuItem key={lang} value={lang}>
+                              <Checkbox checked={language.indexOf(lang) > -1} />
+                              <ListItemText primary={lang} />
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      </FormControl>
+                    </FormControl>
+                    <FormControl fullWidth sx={{ mt: 2 }}>
+                      <InputLabel id="demo-simple-select-label">Gender</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={gender}
+                        label="Gender"
+                        onChange={handleGender}
+                      >
+                        <MenuItem value={'male'}>Male</MenuItem>
+                        <MenuItem value={'female'}>Female</MenuItem>
+                        <MenuItem value={'other'}>Other</MenuItem>
+                        <MenuItem value={'Prefer not to say'}>Prefer not to say</MenuItem>
+                      </Select>
+                    </FormControl>
+                    {updated && <Typography sx={{color:'green', mt:1}}>Successfully Updated!</Typography>}
+                    {error && <Typography sx={{color:'red', mt:1}}>{error}</Typography>}
+                  </>
+                  )
+                }
               </Box>
             </DialogContent>
             <DialogActions>
--- src/Service/LabourHomeService.js
+++ src/Service/LabourHomeService.js
@@ -1,10 +1,27 @@
-import { Email } from "@mui/icons-material";
 import axiosAuthInstance from "./AuthService";
+import axios from "axios";
 
 const REST_API_BASE_URL_LABOUR_PROFILE = "http://localhost:8080/api/labour-profiles";
+const REST_API_BASE_URL_LABOUR_REVIEW = "http://localhost:8080/api/v1/labourReview";
 
 export const getLabourProfileById = (email) => {
-    return axiosAuthInstance.get(`${REST_API_BASE_URL_LABOUR_PROFILE}/getLabourProfileById/${email}`)
+    return axios.get(`${REST_API_BASE_URL_LABOUR_PROFILE}/getLabourProfileById/${email}`)
 }
 
+// get my reviews
+export const getMyReviews = () => {
+    return axiosAuthInstance.get(`${REST_API_BASE_URL_LABOUR_REVIEW}/getMyReviews`)
+}
+
+// Get avg rating
+export const getAvgRating= () => {
+    return axiosAuthInstance.get(`${REST_API_BASE_URL_LABOUR_REVIEW}/rating`)
+}
+
+// Update profile
+export const updateProfile = (email, aboutMe, gender, languages) => {
+    return axios.put(`${REST_API_BASE_URL_LABOUR_PROFILE}/update/${email}`,{
+        email, aboutMe, gender, languages
+    })
+}
 
