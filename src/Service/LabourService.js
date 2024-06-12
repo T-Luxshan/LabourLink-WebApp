9b52bedf0df50063a6db3a7d589f0c767b624503
T-Luxshan <luckybraveboys@gmail.com>
2024-06-12 18:46:01 +0530

    Update password APIadded

--- src/Service/LabourService.js
+++ src/Service/LabourService.js
@@ -5,3 +5,9 @@ const REST_API_BASE_URL = "http://localhost:8080/api/labour";
 export const getLabourById = (email) => {
   return axiosAuthInstance.get(REST_API_BASE_URL + "/getLabourById/" + email);
 };
+
+export const updatePassword = (email, newPassword) => {
+  return axiosAuthInstance.put(`${REST_API_BASE_URL}/changePassword/${email}`, {
+    newPassword
+  })
+}
