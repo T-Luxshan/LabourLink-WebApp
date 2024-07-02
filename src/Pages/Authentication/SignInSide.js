2ca13313ca9acf853289971abfde17edc37d8d17
T-Luxshan <luckybraveboys@gmail.com>
2024-07-02 13:48:13 +0530

    remember me checkbox removed

--- src/Pages/Authentication/SignInSide.js
+++ src/Pages/Authentication/SignInSide.js
@@ -192,10 +192,10 @@ const SignInSide = () => {
                 id="password"
                 autoComplete="current-password"
               />
-              <FormControlLabel
+              {/* <FormControlLabel
                 control={<Checkbox value="remember" color="primary" />}
                 label="Remember me"
-              />
+              /> */}
               <Button
                 type="submit"
                 fullWidth
